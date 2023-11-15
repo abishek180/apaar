cfc41b1db182c005231f482b56af189429c3a0b9
Abi <johndoe@example.com>
2023-11-15 12:54:24 +0530

    deleted one file

--- src/app/components/Client.js
+++ src/app/components/Client.js
@@ -10,8 +10,8 @@ export const Client = () => {
       <div className=" md:text-[24px] sm:text-[16px] font-bold leading-normal  mt-[150px] flex justify-center">
         <span>Our Trusted Users</span>
       </div>
-      <div className="flex justify-evenly md:gap-[115.082px] gap-5 mt-20">
-        <div className="md:flex md:justify-around md:gap-[115.082px]">
+      <div className="md:flex md:flex-row md:justify-evenly md:gap-[115.082px] gap-5 mt-20 flex flex-col">
+        <div className="flex flex-row justify-around  md:gap-[115.082px]">
           <div className="flex">
             <Image src={nmsdubai} className="md:w-[67.77px] md:h-[70.328px] w-[43.369px] h-[45.005px]"/>
             <div>
@@ -19,7 +19,7 @@ export const Client = () => {
               <p className="text-[#000] md:text-[21.738px] text-[13.911px] font-bold">DUBAI</p>
             </div>
           </div>
-          <div className="flex md:mt-0 mt-10">
+          <div className="flex">
             <Image src={layer} className="md:w-[98.051px] md:h-[77.239px] w-[71.189px] h-[48.324px]"/>
             <div>
               <p className="text-[#113299] md:text-[51.995px] text-[32.53px]">ACE</p>
@@ -29,7 +29,7 @@ export const Client = () => {
             </div>
           </div>
         </div>
-        <div className="md:flex md:justify-around md:gap-[115.082px]">
+        <div className="flex flex-row justify-around md:gap-[115.082px] md:mt-0 my-4">
           <div className="flex">
             <Image src={oxford} className="md:w-[72.885px] md:h-[78px] w-[42.984px] h-[46px]" />
             <div className="mt-3">
@@ -39,7 +39,7 @@ export const Client = () => {
               <p className="text-[#000] md:text-[16.623px] text-[9.803px] font-bold">CALICUT</p>
             </div>
           </div>
-          <div className="flex md:mt-0 mt-10">
+          <div className="flex">
             <Image src={oxford} className="md:w-[72.885px] md:h-[78px] w-[42.984px] h-[46px]"/>
             <div className="mt-3">
               <p className="text-[#113299] md:text-[20.459px] text-[12.066px] font-extrabold">
--- src/app/page.js
+++ src/app/page.js
@@ -24,7 +24,7 @@ export default function Home() {
       <CTA />
       <Business />
       <Footer />
-      {/* <NavBar2/> */}
+      
     </div>
   );
 }
